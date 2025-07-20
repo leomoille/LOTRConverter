cbed281a80a5d9c250b6c02b567f0d2dd45693b2
Léo Moille <leomoille18@gmail.com>
2025-07-20 18:37:41 +0200

    Add some stacks, texts and images

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -11,11 +11,22 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 // Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    
                 
                 // Conversion section
                 HStack {
@@ -24,29 +35,54 @@ struct ContentView: View {
                         // Currency
                         HStack {
                             // Currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             // Currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         // Text field
+                        Text("Text field")
                     }
                     
                     // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right conversion section
                     VStack {
                         // Currency
                         HStack {
                             // Currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             // Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         // Text field
+                        Text("Text field")
                     }
                 }
                 
+                Spacer()
+                
                 // Info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
         }
     }
