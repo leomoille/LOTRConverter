64bfdc157be852fe52772b2f27d20d4067a34dc9
Léo Moille <leomoille18@gmail.com>
2025-07-20 17:59:56 +0200

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Léo Moille on 20/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
