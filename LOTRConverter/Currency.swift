7d0bc0725b6276f5b6bb91e8049b25c4c34bd5cf
Léo Moille <leomoille18@gmail.com>
2025-07-21 08:15:41 +0200

    Add Currency enum to represent LOTR coin values

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Léo Moille on 21/07/2025.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
---
+++ Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  LOTRConverter
+//
+//  Created by Léo Moille on 21/07/2025.
+//
+
